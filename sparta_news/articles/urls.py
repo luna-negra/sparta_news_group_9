a9fa2338767519fb1209fbfb3594930e79bf4c74
soyeongpark2090 <syp4546@gmail.com>
2024-05-03 17:49:26 +0900

    댓글 생성

--- sparta_news/articles/urls.py
+++ sparta_news/articles/urls.py
@@ -7,4 +7,5 @@ app_name: str = "articles"
 urlpatterns: list = [
     path("", test),
     path("comments/<int:article_id>",views.CommentListCreatView.as_view()),
+    # path("comments/detail/<int:comment_id>",views.CommentDetailView.as_view()),
 ]
--- sparta_news/articles/views.py
+++ sparta_news/articles/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
-from .models import Comments
+from .models import Comments, Articles, Accounts
 from .serializers import CommentSerializer
 
 
@@ -21,5 +21,51 @@ class CommentListCreatView(generics.ListAPIView):
         return Response(serializer.data)
 
 
-    def post(self,request,article_id): 
-        pass
+    def post(self,request,article_id):
+        # user = request.user
+        user = get_object_or_404(Accounts, pk=1)
+        article = get_object_or_404(Articles, pk=article_id)
+
+        content = request.data.get("content")
+
+        if not content:
+            return Response({"error":"댓글 내용 입력이 없습니다"})
+        
+        if len(content) < 10:
+            return Response({"error":"댓글의 내용을 10글자 이상 작성해주세요"})
+        
+        comment = Comments.objects.create(content=content, user=user, article=article)
+        serializer = CommentSerializer(comment)
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    
+
+class CommentDetailView(generics.ListAPIView):
+    serializer_class = CommentSerializer
+
+    def put(self,request,comment_id):
+        user = get_object_or_404(Accounts, pk=1)
+        comment = get_object_or_404(Comments,pk=comment_id)
+        content = request.data.get("content")
+        if not content:
+            return Response({"error":"댓글 내용 입력이 없습니다"})
+        
+        if len(content) < 10:
+            return Response({"error":"댓글의 내용을 10글자 이상 작성해주세요"})
+        
+        #if request.user == comment.user:
+        if user == comment.user:
+            comment.content = content
+            comment.save()
+            serializer = CommentSerializer(comment)
+            return Response(serializer.data)
+        return Response({"error":"권한 없는 사용자입니다."},status=status.HTTP_403_FORBIDDEN)
+
+
+    def delete(self,request,comment_id):
+        user = get_object_or_404(Accounts, pk=1)
+        comment = get_object_or_404(Comments, pk=comment_id)
+        #if request.user == comment.user:
+        if user == comment.user:
+            comment.delete()
+            return Response({"message":"댓글이 삭제 되었습니다."},status=status.HTTP_204_NO_CONTENT)
+        return Response({"error":"권한 없는 사용자입니다."},status=status.HTTP_403_FORBIDDEN)
