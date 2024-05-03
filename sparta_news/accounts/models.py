b4dfcf3684b13c1ab515fb78d4243611571c8476
Pavel <blitzangreiffer@gmail.com>
2024-05-03 13:01:33 +0900

    dev #1
    1.  환경 구성 완료
    
    자세한 내용은 Project 탭 내 '1. 프로젝트준비' Ticket을 참조해주세요!
    
    https://github.com/users/luna-negra/projects/10/views/1?pane=issue&itemId=61752272

---
+++ sparta_news/accounts/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+
+class Accounts(AbstractUser):
+
+    email = models.EmailField(unique=True,
+                              blank=False,
+                              null=False
+                              )
+    introduction = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField()
+
+    class Meta:
+        db_table: str = "Accounts"
