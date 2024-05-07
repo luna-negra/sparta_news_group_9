2326f1368af80bbe91678c4f7235f0933dc0070a
Pavel <blitzangreiffer@gmail.com>
2024-05-07 16:00:01 +0900

    dev #3
    1. 통합 Branch 테스트 및 버그 확인
    
    -  accounts:  테스트 중 로그아웃 계정의 비밀번호 변경 및 사용자 정보
       수정 이슈 해결
    
    -  articles:  conflict 해결 이후 나타난 코드 변수명 불일치 수정 및 누락
       변수명 추가

--- sparta_news/articles/views.py
+++ sparta_news/articles/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import Comments, Articles, Accounts
