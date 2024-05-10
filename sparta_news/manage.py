bb0be6101f93495c359d7086ec1fb848d931addb
Pavel <blitzangreiffer@gmail.com>
2024-05-10 11:35:40 +0900

    feature/hiding_secretkey #2
    1. SECRET_KEY가 자동 생성되도록 코드 수정.
    
    -  서버 재실행 시, SECRET_KEY 자동 갱신
    -  서버 구동을 위한 별도의 설정 불필요.

--- sparta_news/manage.py
+++ sparta_news/manage.py
@@ -2,9 +2,23 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from django.core.management.utils import get_random_secret_key
+
+
+def generate_secret_key():
+    return get_random_secret_key()
+
+
+def update_secret_key():
+    # Check if SECRET_KEY is missing or empty
+    if not os.getenv('SECRET_KEY'):
+        os.environ.setdefault(key='SECRET_KEY',
+                              value=generate_secret_key())
 
 
 def main():
+    update_secret_key()
+
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sparta_news.settings")
     try:
